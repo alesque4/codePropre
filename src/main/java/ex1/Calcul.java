0dd1cee6277936c2fa231a9091d4dff9786e4b9f
Alexandre MEUR <alex.meur@gmail.com>
2019-05-24 12:35:38 +0200

    Exo1 et 2

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,29 @@
+package ex1;
+
+/**
+ * Classe qui réalise des opérations arythmétiques
+ * @author DIGINAMIC
+ */
+public class Calcul {
+
+	/** Méthode qui retourne l'addition des paramètres a et b 
+	 * 
+	 * @param a paramÃ¨tre a
+	 * @param b paramÃ¨tre b
+	 * @return int a+b
+	 */
+	public int addition(int a, int b){
+		return a+b;
+	}
+	
+	/**
+	 * Méthode qui retourne la soustraction des paramètres a et b
+	 * 
+	 * @param a
+	 * @param b
+	 * @return int a-b
+	 */
+	public int soustraction(int a, int b){
+		return a-b;
+	}
+}
--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,28 @@
 package ex2;
 
 public class LivretA extends CompteBancaire {
+	
+	private final static int DECOUVERT_LIVRET = 0;
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+	/** tauxRemuneration : taux de rémunération dans le cas d'un livret A */
+	private double tauxRemuneration;
+	
+	public LivretA(double solde, double tauxRemuneration) {
+		super(solde, DECOUVERT_LIVRET);
+		this.tauxRemuneration = tauxRemuneration;
+	}
+	
+	/** Getter for tauxRemuneration
+	 * @return the tauxRemuneration
+	 */
+	public double getTauxRemuneration() {
+		return tauxRemuneration;
+	}
+	/** Setter
+	 * @param tauxRemuneration the tauxRemuneration to set
+	 */
+	public void setTauxRemuneration(double tauxRemuneration) {
+		this.tauxRemuneration = tauxRemuneration;
 	}
 
 }
